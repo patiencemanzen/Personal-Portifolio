d3b528e50ddf2757af76898486d507f519f48e62
manirabona-programer <hseal419@gmail.com>
2023-06-15 18:12:45 +0200

    chore(meta-tags): added social graph meta tags

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -13,6 +13,110 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" type="image/x-icon" href="/img/logos/logo-black.png">
 
+    <meta name="theme-color" content="#1e2a3b">
+
+    <!-- Basic HTML Meta Tags -->
+    <meta charset='UTF-8'>
+    <meta name="googlebot" content="noodp" />
+    <meta property="article:published_time" content="Thu Jun 15 2023">
+    <meta property="article:author" content="{{ env('APP_URL') }}">
+    <meta name="robots" content="index,follow,max-image-preview:large">
+    <meta name="description" content="Hi, i'm Manirabona Patience, am full-stack Engineer with Many years of experience in JavaScript, PHP, and SQL including some libraries and frameworks like In JavaScript(React js, vue js, node Js), and Also MongoDB, PHP (laravel and Symfony)">
+    <meta name='subject' content="Manirabona Patience's portifolio">
+    <meta name='copyright' content='Manirabona Patience'>
+    <meta name='language' content='ES'>
+    <meta name='robots' content='index,follow'>
+    <meta name='revised' content='Sunday, June 12th, 2023'>
+    <meta name='abstract' content=''>
+    <meta name='topic' content="Manirabona Patience's Portifolio">
+    <meta name='summary' content="Hi, i'm Manirabona Patience, am full-stack Engineer with Many years of experience in JavaScript, PHP, and SQL including some libraries and frameworks like In JavaScript(React js, vue js, node Js), and Also MongoDB, PHP (laravel and Symfony)">
+    <meta name='Classification' content='Portifolio'>
+    <meta name='author' content='Manirabona Patience, hseal419@gmail.com'>
+    <meta name='designer' content='Manirabona Patience'>
+    <meta name='reply-to' content='hseal419@gmail.com'>
+    <meta name='owner' content='Manirabona Patience'>
+    <meta name='url' content='{{ env('APP_URL') }}'>
+    <meta name='identifier-URL' content='{{ env('APP_URL') }}'>
+    <meta name='directory' content='submission'>
+    <meta name='pagename' content="Hi, i'm Manirabona Patience, am full-stack Engineer with Many years of experience in JavaScript, PHP, and SQL including some libraries and frameworks like In JavaScript(React js, vue js, node Js), and Also MongoDB, PHP (laravel and Symfony)">
+    <meta name='category' content=''>
+    <meta name='coverage' content='Worldwide'>
+    <meta name='distribution' content='Global'>
+    <meta name='rating' content='General'>
+    <meta name='revisit-after' content='7 days'>
+    <meta name='subtitle' content="Hi, i'm Manirabona Patience, am full-stack Engineer with Many years of experience in JavaScript, PHP, and SQL including some libraries and frameworks like In JavaScript(React js, vue js, node Js), and Also MongoDB, PHP (laravel and Symfony)">
+    <meta name='target' content='all'>
+    <meta name='HandheldFriendly' content='True'>
+    <meta name='MobileOptimized' content='320'>
+    <meta name='date' content='June. 12, 2023'>
+    <meta name='search_date' content='2023-06-12'>
+    <meta name='DC.title' content="Hi, i'm Manirabona Patience, am full-stack Engineer with Many years of experience in JavaScript, PHP, and SQL including some libraries and frameworks like In JavaScript(React js, vue js, node Js), and Also MongoDB, PHP (laravel and Symfony)">
+    <meta name='ResourceLoaderDynamicStyles' content=''>
+    <meta name='medium' content='portifolio'>
+    <meta name='pageKey' content='portifolio'>
+    <meta http-equiv='Expires' content='0'>
+    <meta http-equiv='Pragma' content='no-cache'>
+    <meta http-equiv='Cache-Control' content='no-cache'>
+    <meta http-equiv='imagetoolbar' content='no'>
+    <meta http-equiv='x-dns-prefetch-control' content='off'>
+
+    <!-- OpenGraph Meta Tags -->
+    <meta property='og:title' content="Manirabona Patience: Extraordinary individual known as developer">
+    <meta property='og:type' content='potifolio'>
+    <meta property='og:url' content='{{ env('APP_URL') }}'>
+    <meta property='og:image' content='{{ env('APP_URL')."/img/profile.png" }}'>
+    <meta property='og:site_name' content='Patienceman-blogs'>
+    <meta property='og:description' content="Hi, i'm Manirabona Patience, am full-stack Engineer with Many years of experience in JavaScript, PHP, and SQL including some libraries and frameworks like In JavaScript(React js, vue js, node Js), and Also MongoDB, PHP (laravel and Symfony)">
+
+    <meta property='fb:page_id' content='43929265776'>
+    <meta property='application-name' content='Manirabona Patience'>
+    <meta property='og:email' content='hseal419@gmail.com'>
+
+    <meta property='og:latitude' content='-1.9584038'>
+    <meta property='og:longitude' content='30.0694946'>
+    <meta property='og:street-address' content='34 KN 41 Street'>
+    <meta property='og:locality' content='Nyarugenge'>
+    <meta property='og:region' content='NY'>
+    <meta property='og:postal-code' content='5748'>
+    <meta property='og:country-name' content='RW'>
+
+    <meta property='twitter:card' content='summary_large_image'>
+    <meta property='twitter:site' content="Manirabona Patience's portifolio">
+    <meta property='twitter:creator' content='Manirabona Patience'>
+    <meta property='twitter:description' content="Hi, i'm Manirabona Patience, am full-stack Engineer with Many years of experience in JavaScript, PHP, and SQL including some libraries and frameworks like In JavaScript(React js, vue js, node Js), and Also MongoDB, PHP (laravel and Symfony)">
+    <meta property='twitter:title' content="Manirabona Patience: Extraordinary individual known as developer">
+    <meta property='twitter:image' content='{{ env('APP_URL')."/img/profile.png" }}'>
+    <meta property='twitter:image:alt' content="Hi, i'm Manirabona Patience, am full-stack Engineer with Many years of experience in JavaScript, PHP, and SQL including some libraries and frameworks like In JavaScript(React js, vue js, node Js), and Also MongoDB, PHP (laravel and Symfony)">
+    <meta property="twitter:image:src" content="{{ env('APP_URL')."/img/profile.png" }}">
+
+    <!-- App meta tags -->
+    <meta name="apple-mobile-web-app-title" content="Hi, i'm Manirabona Patience, am full-stack Engineer with Many years of experience in JavaScript, PHP, and SQL including some libraries and frameworks like In JavaScript(React js, vue js, node Js), and Also MongoDB, PHP (laravel and Symfony)">
+    <meta name='apple-mobile-web-app-capable' content='yes'>
+    <meta name='apple-touch-fullscreen' content='yes'>
+    <meta name='apple-mobile-web-app-status-bar-style' content='black'>
+    <meta name='format-detection' content='telephone=no'>
+    <meta name='viewport' content='width=device-width; content='width = 320; initial-scale=1.0; maximum-scale=1.0; user-scalable=yes; target-densitydpi=160dpi'>
+
+    <!-- Internet Explore -->
+    <meta http-equiv='Page-Enter' content='RevealTrans(Duration=2.0,Transition=2)'>
+    <meta http-equiv='Page-Exit' content='RevealTrans(Duration=3.0,Transition=12)'>
+    <meta name='mssmarttagspreventparsing' content='true'>
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible"/>
+    <meta name='msapplication-starturl' content='{{ env('APP_URL') }}'>
+    <meta name='msapplication-window' content='width=800;height=600'>
+    <meta name='msapplication-navbutton-color' content='red'>
+    <meta name='application-name' content="Hi, i'm Manirabona Patience, am full-stack Engineer with Many years of experience in JavaScript, PHP, and SQL including some libraries and frameworks like In JavaScript(React js, vue js, node Js), and Also MongoDB, PHP (laravel and Symfony)">
+    <meta name='msapplication-tooltip' content="Manirabona Patience's Portifolio">
+    <meta name='msvalidate.01' content='6E3AD52DC176461A3C81DD6E98003BC9'>
+    <meta http-equiv='cleartype' content='on'>
+
+    <!-- Google meta tags -->
+    <meta name="news_keywords" content="Hi, i'm Manirabona Patience, am full-stack Engineer with Many years of experience in JavaScript, PHP, and SQL including some libraries and frameworks like In JavaScript(React js, vue js, node Js), and Also MongoDB, PHP (laravel and Symfony)">
+
+    <!-- TweetMeme -->
+    <meta name='tweetmeme-title' content="Hi, i'm Manirabona Patience, am full-stack Engineer with Many years of experience in JavaScript, PHP, and SQL including some libraries and frameworks like In JavaScript(React js, vue js, node Js), and Also MongoDB, PHP (laravel and Symfony)">
+    <meta data-rh="true" name="referrer" content="safe-url">
+
     <title> @yield('title') | portifolio | patienceman </title>
 
     @livewireStyles
