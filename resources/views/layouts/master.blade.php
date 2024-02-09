1a3b2c7ece2b7167426114e611615aa3f5296a94
Manirabona Patience <hseal419@gmail.com>
2024-02-09 09:37:40 +0200

    chore: set default color theme

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -142,9 +142,12 @@
         // On page load or when changing themes, best to add inline in `head` to avoid FOUC
         if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia(
                 '(prefers-color-scheme: dark)').matches)) {
-            document.documentElement.classList.add('dark');
+            // document.documentElement.classList.add('dark');
+            document.documentElement.classList.remove('dark')
+            document.documentElement.classList.add('light')
         } else {
             document.documentElement.classList.remove('dark')
+            document.documentElement.classList.remove('light')
         }
     </script>
 
